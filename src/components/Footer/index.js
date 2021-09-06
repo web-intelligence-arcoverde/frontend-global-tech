e13fa9600b25ae3e77975621da17ec7105c78c30
Paulinho Marques <paulinho_marques2012@hotmail.com>
2021-09-06 15:24:22 -0300

    Footer quase finalizado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,46 @@
 import react from "react";
-import * as S from './styled'
+import * as S from './styled';
+import Facebook from '../../assets/facebook.png';
+import Linkedin from '../../assets/inkedin.png';
+import Intagram from '../../assets/instagram.png';
+import Gps from '../../assets/gps.png';
+import Mensagem from '../../assets/mensagem.png';
+import Telefone from '../../assets/telefone2.png';
+
+
+
 const Footer = () => {
 
-    return(
-        <div>
-                <div>
+    return (
+        <S.Main>
+        <S.Container>
+            <S.Card>
+                <h3>Informações</h3>
+                <p>Termos de uso</p>
+                <p>Politica de segurança</p>
+                <p>Sobre nós</p>
+            </S.Card>
 
-                </div>
+            <S.Card>
+                <h3>Comunicação</h3>
+                <p><S.Icones src={Telefone}></S.Icones>+55 87 99105 4786</p>
+                <p><S.Icones src={Mensagem} ></S.Icones>paulinho_marques2012@hotmail.com</p>
+                <p ><S.Icones src={Gps}></S.Icones>Avenida Osvaldo Cruz  1500</p>
+            </S.Card>
 
-                <div>
+            <S.ContainerRedes>
+                <h3>Nossas Redes Sociais</h3>
+                <S.RedesSocias>
+                    <S.Icones src={Facebook} />
+                    <S.Icones src={Linkedin} />
+                    <S.Icones src={Intagram} />
+                </S.RedesSocias>
+            </S.ContainerRedes>
 
-                </div>
 
-                <div>
-
-                </div>
-
-                
-        </div>
+        </S.Container>
+        <S.FooterPara>©Todos os direitos reservados 2021</S.FooterPara>
+        </S.Main>
     )
 
 }
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,1 +1,46 @@
 import styled from "styled-components";
+
+export const Container =styled.footer`
+display:flex;
+justify-content:space-between;
+
+padding-right: 30px;
+padding-left: 30px;
+background: #3D3D3D;
+color:white
+`
+
+export const ContainerRedes =styled.div `
+
+align-items: center;
+
+`
+
+export const RedesSocias =styled.div`
+
+display: flex;
+justify-content: space-around;
+`
+export const Icones =styled.img `
+
+width: 30px;
+margin-right: 30px;
+
+`
+export const Card =styled.div`
+align-items: center;
+
+
+`
+
+export const FooterPara =styled.p `
+text-align: center;
+background:#3D3D3D;
+color:white;
+padding: 20px;
+border-top: 1px solid white;
+
+`
+export const Main =styled.div `
+background:#3D3D3D;
+`
