355c23ab1cad88ac5cb42bf0e993143e7761cf69
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-09-16 22:27:16 -0300

    chore: add CI package.json for vercel deploy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,9 @@ import React from "react";
 
 import Menu from "../../components/organisms/Menu";
 import PlansInformation from "../../components/organisms/Plans";
-
 import Secao3 from "../../components/Secao-Tres";
 import Footer from "../../components/Footer";
-
 import Section01 from "../../components/organisms/Section01";
-
 import ReasonsChoiceCards from "../../components/organisms/ReasonChoiceCards";
 
 const Home = () => {
@@ -30,18 +27,8 @@ const Home = () => {
           <header>
             <Menu />
           </header>
-
-          <main>
-            <Section01 />
-            <PlansInformation />
-            <ReasonsChoiceCards />
-            <Secao3 />
-          </main>
         </div>
       </div>
-      <footer>
-        <Footer />
-      </footer>
     </>
   );
 };
